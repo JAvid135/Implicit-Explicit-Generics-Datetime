d78cb1ee80283c52decaa9da080a1d8a968f48c5
unknown <javidra@code.edu.com>
2022-11-05 14:18:44 +0400

    NewHomework

---
+++ CelsiusKelvin/CelsiusKelvin/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CelsiusKelvin
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Kelvin kelvin = new Kelvin(12.3);
+            Celsius celsius = kelvin;
+            Console.WriteLine(celsius.Cels);
+            
+        }
+    }
+
+    public class Celsius
+    {
+        public double Cels { get; set; }
+
+        public Celsius(double cels)
+        {
+            Cels = cels;
+        }
+    }
+
+    public class Kelvin
+    {
+        public double Kel { get; set; }
+
+        public Kelvin(double kel)
+        {
+
+            Kel = kel;
+        }
+
+        public static implicit operator Celsius(Kelvin kelvin)
+        {
+            Celsius celsius = new Celsius(kelvin.Kel + 273);
+            return celsius;
+            
+        }
+
+    }
+}
---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Practice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Manat manat = new Manat(100);
+            //Dollar dollar = manat;
+            //Console.WriteLine(dollar.Usd);
+
+            //DateTime dateTime = DateTime.Now;
+            //Console.WriteLine(dateTime.ToString("MM-dd-yyyy"));
+
+            //DateTime dateTime = DateTime.UtcNow;
+            //Console.WriteLine(dateTime.ToString("MM-dd-yyyy"));
+
+
+
+        }
+    }
+
+    public class StringList
+    {
+        private string[] _arr;
+
+        public StringList()
+        {
+            _arr = new string[0];
+        }
+
+        public void Add(string str)
+        {
+            Array.Resize(ref _arr, _arr.Length + 1);
+            _arr[_arr.Length + 1] = str;
+        }
+
+        int[] test = { 1, 2, 3 };
+    }
+
+
+
+    public class Dollar
+    {
+        public double Usd { get; set; }
+
+        public Dollar(double usd)
+        {
+            Usd = usd;
+        }
+    }
+
+    public class Manat
+    {
+        public double Azn { get; set; }
+
+        public Manat(double azn)
+        {
+            Azn = azn;
+        }
+
+        public static implicit operator Dollar(Manat manat)
+        {
+            //Dollar dollar = new Dollar(manat.Azn / 1.7);
+            return new Dollar(manat.Azn / 1.7);
+        }
+    }
+}
